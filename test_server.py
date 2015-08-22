86a5083754e27305a7329095fa8826ba8f241c8e
Thomas Zakrajsek <tzakrajs@gmail.com>
2015-08-22 19:25:18 -0400

    initial commit

---
+++ test_server.py
@@ -0,0 +1,21 @@
+from aiobottle import AsyncBottle
+import aiohttp
+
+app = AsyncBottle()
+
+@app.get('/')
+def example():
+    rsp = yield from aiohttp.request('GET', 'http://python.org')
+    content = yield from rsp.read()
+    return bytes(content)
+
+@app.get('/nomal')
+def nomal():
+    return 'hello word!'
+
+def main(host='::', port=8080):
+    from bottle import run
+    run(app, host = host, port = port, server = 'aiobottle:AsyncServer')
+
+if __name__ == '__main__':
+    main()
