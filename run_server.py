e6a4858bdcc88c0a7bbc99dc806f6a779915acf6
Thomas Zakrajsek <tzakrajs@gmail.com>
2015-08-22 16:58:43 -0700

    Adding initial scaffolding

--- run_server.py
+++ run_server.py
@@ -1,19 +1,18 @@
 from aiobottle import AsyncBottle
 import aiohttp
+from bottle import jinja2_view, request
 
+# Initialize aiobottle app
 app = AsyncBottle()
 
 @app.get('/')
-def example():
-    rsp = yield from aiohttp.request('GET', 'http://python.org')
-    content = yield from rsp.read()
-    return bytes(content)
-
-@app.get('/nomal')
-def nomal():
-    return 'hello word!'
+@jinja2_view('home.html', template_lookup=['views'])
+def home():
+    """Render home page"""
+    return {'env': str(request.environ)}
 
 def main(host='::', port=8080):
+    """Start Bottle server"""
     from bottle import run
     run(app, host = host, port = port, server = 'aiobottle:AsyncServer')
 
