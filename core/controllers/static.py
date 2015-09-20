6f1be82dba03476fc5b0257dfc9c7f01c030a99b
Thomas Zakrajsek <tzakrajs@gmail.com>
2015-09-20 15:29:06 -0400

    add config file and implemented options in run_server, removed old app.index

--- core/controllers/static.py
+++ core/controllers/static.py
@@ -5,8 +5,9 @@ from core import route
 # Static Assets
 route._routes.append(['/assets/(.*)', tornado.web.StaticFileHandler, {'path': 'core/static/assets/'}])
 route._routes.append(['/app/(.*)', tornado.web.StaticFileHandler, {'path': 'core/static/app/'}])
+
+# Index for Angular Single Page
 @route('/')
 class HomePage (tornado.web.RequestHandler):
     def get(self):
-        home = '\n'.join(open('core/static/app/app.index.html', 'r').readlines())
-        self.write(home)
+        self.render('../static/app/app.index.html')
--- run_server.py
+++ run_server.py
@@ -1,43 +1,88 @@
 #!/usr/bin/python3
 
+import sys
 import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
-from core import route
- 
-@route('/ws')
-class WSHandler(tornado.websocket.WebSocketHandler):
-    def open(self):
-        print('new connection')
-      
-    def on_message(self, message):
-        print('message received:  %s' % message)
-        # Reverse Message and send it back
-        print('sending back message: %s' % message[::-1])
-        self.write_message(message[::-1])
- 
-    def on_close(self):
-        print('connection closed')
- 
-    def check_origin(self, origin):
-        return True
+import yaml
 
 from core import logging, main_loop, route
 
-class RedirectToSSL(tornado.web.RequestHandler):
-    def get(self, path):
-        self.redirect('https://www.cloud-fortress.com:9999/{}'.format(path))
+# YAML Config is located at the path below
+YAML_CONFIG_PATH = './config.yml'
 
-redirect_app = tornado.web.Application([(r"/(.*)", RedirectToSSL),],)
+# Define https/wss server
+application = tornado.web.Application(route.get_routes())
+
+def generate_redirect_app(hostname='localhost', port=443):
+    if port == 443:
+        host = hostname
+    else:
+        host = '{0}:{1}'.format(hostname, port)
+    # Define http/ws server (only redirect to https/wss)
+    class RedirectToSSL(tornado.web.RequestHandler):
+        def get(self, path):
+            self.redirect('https://{0}/{1}'.format(host, path))
+    return tornado.web.Application([(r"/(.*)", RedirectToSSL),],)
+
+# Load the configuration file
+try:
+    config_file = open(YAML_CONFIG_PATH, 'r')
+except FileNotFoundError:
+    error_message = "YAML config not found: {0}".format(YAML_CONFIG_PATH)
+    logging.error(error_message)
+    sys.exit(error_message)
+config = yaml.load(config_file)
 
 if __name__ == "__main__":
-    http_server = tornado.httpserver.HTTPServer(redirect_app)
-    http_server.listen(8888, '::')
-    ssl_options = {'certfile': '/etc/ssl/certs/ssl-cert-snakeoil.pem',
-                   'keyfile': '/etc/ssl/private/ssl-cert-snakeoil.key'}
-    https_server = tornado.httpserver.HTTPServer(application, ssl_options=ssl_options)
-    https_server.listen(9999, '::')
-    tornado.ioloop.IOLoop.configure('tornado.platform.asyncio.AsyncIOLoop')
-    tornado.ioloop.IOLoop.current().start()
-    tornado.ioloop.IOLoop.instance().start()
+    # Load the top level config dictionaries
+    general_config = config.get('general', {})
+    non_ssl_config = config.get('non_ssl', {})
+    ssl_config = config.get('ssl', {})
+    # Set our listening IP addresses and ports
+    ipv4_ip = general_config.get('ipv4_ip')
+    ipv6_ip = general_config.get('ipv6_ip')
+    http_port = non_ssl_config.get('port', 8080)
+    https_port = ssl_config.get('port', 8443)
+    # Non SSL
+    if non_ssl_config.get('enabled'):
+        # Setup the http/ws server
+        if non_ssl_config.get('redirect_to_ssl'):
+            # This one only redirects to https, ws does not and cannot be
+            # redirected since browsers disallow ws requests originating from
+            # https websites
+            hostname = general_config.get('hostname')
+            external_port = ssl_config.get('external_port', https_port)
+            redirect_app = generate_redirect_app(hostname, external_port)
+            http_server = tornado.httpserver.HTTPServer(redirect_app)
+        else:
+            # This one is the full application
+            http_server = tornado.httpserver.HTTPServer(application)
+        if ipv6_ip:
+            # Listen on IPv6 address
+            http_server.listen(http_port, ipv6_ip)
+        if ipv4_ip:
+            # Listen on IPv4 address
+            http_server.listen(http_port, ipv4_ip)
+    # SSL
+    if ssl_config.get('enabled'):
+        # Setup https/wss server
+        ssl_crt = ssl_config.get('crt_path')
+        ssl_key = ssl_config.get('key_path')
+        try:
+            ssl_options = {'certfile': ssl_crt,
+                           'keyfile': ssl_key}
+            https_server = tornado.httpserver.HTTPServer(application, ssl_options=ssl_options)
+        except Exception as e:
+            error_message = "Unable to setup SSL: {0}".format(e)
+            logging.error(error_message)
+            sys.exit(error_message)
+        if ipv6_ip:
+            # Listen on IPv6 address
+            https_server.listen(https_port, ipv6_ip)
+        if ipv4_ip:
+            # Listen on IPv4 address
+            https_server.listen(https_port, ipv4_ip)
+    # Initialize Main Loop
+    main_loop.start()
