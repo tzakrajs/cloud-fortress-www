f92f148f3949455153e9f229802f707a98e4b41e
Thomas Zakrajsek <tzakrajs@gmail.com>
2015-09-20 15:25:51 -0400

    add logging for core

--- core/__init__.py
+++ core/__init__.py
@@ -1,8 +1,13 @@
 from aiobottle import AsyncBottle
+import logging
 import os
 
-core_path = os.path.dirname(os.path.abspath(__file__))
-static_path = '{}/static'.format(core_path)
+# config log format
+format="%(asctime)-21s %(levelname)s %(name)s (%(funcName)-s) " + \
+       "%(process)d:%(thread)d - %(message)s"
+logging.basicConfig(level=logging.DEBUG,
+                    format=format,
+                    filename='./cloud-fortress-www.log')
 
 from tornroutes import route
 
